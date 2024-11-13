82137e07b0011a3e977fd240d183bcfcd64d92d1
Micaela Rosadio Cayllahua <rosadio.karen@gmail.com>
2024-11-13 16:51:54 +0100

    Style container summit info for mobile

--- modules/desktop/summitUtils.js
+++ modules/desktop/summitUtils.js
@@ -17,36 +17,55 @@ export function displaySummitsCountry(country, summitsCountry) {
     }
     const filterFlag = flagSrc.filter(item => item.country === country);
     const flagCountry = filterFlag[0].img["img-src"];
-    console.log('summits country reversed', summitsCountry.reverse);
-    console.log('Number of summits', summitsCountry.length)
     const noSummits = summitsCountry.length;
     const summitListContainer = document.getElementById('summitList');
     console.log('Summit list container', summitListContainer);
     const hostCountry = document.getElementById('summitCountry');
+    hostCountry.classList.add('h4', 'fw-bold');
     country = country;
-    hostCountry.innerHTML = `<img src="/src/img/country-flags-main/png100px/${flagCountry}" class="img-fluid country-flag border border-dark border-1 rounded me-2" alt="Flag"> ${country}`;
+    hostCountry.innerHTML = `<img src="/src/img/country-flags-main/png100px/${flagCountry}" class="img-fluid country-flag border border-dark border-1 rounded me-2 d-inline" alt="Flag"> ${country}`;
+    
+    // Create and insert the separator element for mobile devices
+    const separatorContainer = document.querySelector('.apri-separator-vis');
+    if (separatorContainer) {
+        separatorContainer.remove();
+    }
+    const separator = document.createElement('div');
+    separator.className = 'apri-separator-vis d-md-none mb-2';
+    hostCountry.insertAdjacentElement('afterend', separator);
+    
     const totalSummits = document.getElementById('total-summit');
-    totalSummits.innerHTML = `Total summits hosted: ${noSummits}`
-    totalSummits.classList.add('fw-bolder')
-    const listSummit = document.getElementById('intro-list-summit');
-    listSummit.innerHTML = 'List of summits:';
-    listSummit.classList.add('fw-bolder', 'mb-0')
+    totalSummits.innerHTML = `${noSummits} summit(s) hosted:`;
+    totalSummits.classList.add('fw-bolder', 'h5');
     const listOrder = document.getElementById('list-order');
-    listOrder.innerHTML = `<i>(from most to least recent)</i>`
+    listOrder.innerHTML = `<i>(from most to least recent)</i>`;
+    listOrder.classList.add('mobile-font-size');
     summitListContainer.innerHTML = '';
 
     // Create a copy of summitsCountry and reverse the copy
     const reversedSummitsCountry = summitsCountry.slice().reverse();
     reversedSummitsCountry.forEach(summit => {
         const listItem = document.createElement('li');
-        let titleContent = summit.title ? `${summit.title}` : `${summit.summitNo} Summit`;
-        let dateContent = summit.date ? `<p class="same-font-color mb-0"><img src="src/img/calendar.svg" class="img-fluid calendar"> ${summit.date}</p>` : '';
-        let placeContent = summit.place ? `<p class="same-font-color"><img src="src/img/map-pin.svg" class="img-fluid location"> ${summit.place}</p>` : '';
-        listItem.innerHTML = `
-            <p class="mb-0 mt-4 same-font-size same-font-color"><span class="badge bg-dark summitNospan">${summit.summitNo}</span>  ${titleContent}</p>
-            ${dateContent}
-            ${placeContent}
-        `;
+    
+    let titleContent = summit.title ? `${summit.title}` : `${summit.summitNo} Summit`;
+    let dateContent = summit.date ? `
+        <div class="d-flex justify-content-start align-items-center mb-0">
+            <img src="/src/img/calendar.svg" class="img-fluid calendar me-3 pe-sm-3">
+            <p class="mobile-font-size mb-0">${summit.date}</p>
+        </div>` : '';
+    let placeContent = summit.place ? `
+        <div class="d-flex justify-content-start align-items-center mb-0">
+            <img src="/src/img/map-pin.svg" class="img-fluid location me-3 pe-sm-5">
+            <p class="mobile-font-size mb-0">${summit.place}</p>
+        </div>` : '';
+    listItem.innerHTML = `
+        <div class="d-flex justify-content-start align-items-start mb-0">
+            <span class="badge badge-mobile bg-dark me-2 pe-sm-5 mobile-font-size badge-normal">${summit.summitNo}</span>
+            <p class="mb-0 mobile-font-size summit-title">${titleContent}</p>
+        </div>
+        ${dateContent}
+        ${placeContent}
+    `;
         summitListContainer.appendChild(listItem);
     })
 }
@@ -54,6 +73,12 @@ export function displaySummitsCountry(country, summitsCountry) {
 export function displaySummitsYear(yearData) {
     console.log('flag', flagSrc);
     console.log('Year data in displaySummitYear', yearData);
+
+    // Empty content that will not be used in this container 
+    const totalSummits = document.getElementById('total-summit');
+    totalSummits.innerHTML = '';
+    const listOrder = document.getElementById('list-order');
+    listOrder.innerHTML = '';
     const hostYear = document.getElementById('summitCountry');
     hostYear.innerHTML = `Summits hosted in ${yearData.year}`;
     hostYear.classList.add('h4', 'fw-bold');
